880d0a3254ee92b9a3eedcd6d8fd7d4a63328bec
Jiacheng Zhang <gh@kt.je>
2021-05-04 13:22:50 +0800

    chore: migrate to gatsby

--- src/utils/web3/index.tsx
+++ src/utils/web3/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useMemo, useRef, useState } from 'react'
+import React, { Suspense, useContext, useMemo, useRef } from 'react'
 import {
   ExtentionContext,
   _defaultContextValue,
@@ -8,17 +8,9 @@ import {
 import { useModal } from '@geist-ui/react'
 
 const _Web3Provider = React.lazy(() => import('./dynamic'))
+
 const Web3Provider: React.FC = ({ children }) => {
   const { current: hasWindow } = useRef(typeof window !== 'undefined')
-  const [_Web3Provider, setDynWeb3Provider] = useState(undefined)
-
-  // useEffect(() => {
-  //   if (hasWindow && !_Web3Provider) {
-  //     ;(async () => {
-  //       setDynWeb3Provider()
-  //     })()
-  //   }
-  // }, [hasWindow])
 
   const accountModal = useModal()
 
@@ -30,13 +22,13 @@ const Web3Provider: React.FC = ({ children }) => {
     }),
     [accountModal]
   )
-  console.log(111,_Web3Provider)
 
   return (
     <>
-      {typeof _Web3Provider !== 'undefined' ? (
+      {hasWindow ? <Suspense fallback={null}>
         <_Web3Provider modal={accountModal}>{children}</_Web3Provider>
-      ) : (
+      </Suspense>
+     : (
         <ExtentionContext.Provider value={value}>
           <AccountModal isEnabled={true} modal={accountModal} />
           {children}
