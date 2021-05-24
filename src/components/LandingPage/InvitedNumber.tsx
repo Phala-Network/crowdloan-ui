92e4157abf1a6f820da9805e7d296daf4a1ff1f1
limichange <limichange@hotmail.com>
2021-05-24 09:01:49 +0800

    fix: invited number size

--- src/components/LandingPage/InvitedNumber.tsx
+++ src/components/LandingPage/InvitedNumber.tsx
@@ -6,12 +6,14 @@ const InvitedNumberRoot = styled.div`
   font-style: normal;
   font-weight: 500;
   font-size: 20px;
-  line-height: 25px;
-  height: 25px;
+  line-height: 38px;
+  height: 38px;
   color: #d9d9d9;
 
   @media (max-width: 768px) {
     font-size: 14px;
+    line-height: 25px;
+    height: 25px;
   }
 `
 
