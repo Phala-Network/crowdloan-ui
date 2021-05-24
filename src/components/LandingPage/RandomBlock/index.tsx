862d77d7405c3b2a369b60611350541760e89a4e
limichange <limichange@hotmail.com>
2021-05-24 09:30:53 +0800

    update: add RandomBlock

---
+++ src/components/LandingPage/RandomBlock/index.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { useMediaQuery } from 'react-responsive'
+
+const RootDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+
+type Props = {
+  row?: number
+  column?: number
+}
+
+const RandomBlock: React.FC<Props> = (props) => {
+  const { row = 8, column = 4 } = props
+  const [size, setSize] = useState(20)
+  const [data, setData] = useState(new Array(row * column).fill(0))
+  const isSM = useMediaQuery({ maxWidth: 960 })
+  const isMD = useMediaQuery({ minWidth: 960, maxWidth: 1280 })
+  const isLG = useMediaQuery({ minWidth: 1280 })
+
+  useEffect(() => {
+    if (isSM) {
+      setSize(10)
+    } else if (isMD) {
+      setSize(15)
+    } else {
+      setSize(20)
+    }
+  }, [isSM, isMD, isLG])
+
+  useEffect(() => {
+    setData(
+      data.map(() => {
+        return Math.random()
+      })
+    )
+  }, [])
+
+  return (
+    <RootDiv
+      style={{
+        width: size * column,
+        height: size * row,
+      }}
+    >
+      {data.map((item, index) => {
+        return (
+          <div
+            style={{
+              width: size,
+              height: size,
+              backgroundColor: item > 0.8 ? '#03ffff' : '',
+            }}
+            key={index}
+          ></div>
+        )
+      })}
+    </RootDiv>
+  )
+}
+
+export default RandomBlock
