bd1eda6f2ae0c82b88db0843009b22e94e4050b1
limichange <limichange@hotmail.com>
2021-05-27 17:22:05 +0800

    update: twitter text i18n

--- src/hooks/useTwitterLink.ts
+++ src/hooks/useTwitterLink.ts
@@ -1,16 +1,13 @@
 import { useWeb3 } from '@/utils/web3'
 import useShareLink from '@/hooks/useShareLink'
-import { useIntl } from 'gatsby-plugin-intl'
+import { useI18n } from '@/i18n'
 
 export default function useTwitterLink(): string {
   const { currentAccount } = useWeb3()
   const shareLink = useShareLink()
-  const { locale } = useIntl()
+  const { t } = useI18n()
   const currentAccountAddress = currentAccount?.address
-  const text =
-    locale === 'en'
-      ? "Khala's participating in the Kusama Parachain Slot Auctions, and you're invited to join in! We'll both get extra rewards if you use my invitate link."
-      : 'Khala将参与Kusama平行链卡槽拍卖，邀请你提前注册！填写我的邀请码将有额外奖励哦！'
+  const text = t('twitterShareText')
 
   if (!currentAccountAddress) return ''
 
