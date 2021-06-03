e17e0b3072eef1858b8e6246c3f52910e3b0db17
limichange <limichange@hotmail.com>
2021-06-03 15:27:17 +0800

    update: medal style

--- src/components/InvitorInfoModal/Medal.tsx
+++ src/components/InvitorInfoModal/Medal.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Medal: React.FC = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '33vh', marginBottom: 22 }}>
+    <div style={{ textAlign: 'center', marginTop: 200, marginBottom: 22 }}>
       <img src="/medal.png" alt="medal" />
     </div>
   )
