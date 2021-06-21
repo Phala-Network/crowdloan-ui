12e225b62733ee2bb3dc8e4a14b32538638baa62
limichange <limichange@hotmail.com>
2021-06-21 20:34:42 +0800

    feat: useActivity22

--- src/hooks/useActivity22.ts
+++ src/hooks/useActivity22.ts
@@ -6,12 +6,9 @@ dayjs.extend(utc)
 dayjs.extend(timezone)
 
 export function useActivity22(): boolean {
-  // const date = dayjs('2021-06-22 12:00').tz('GMT+0')
-  // const now = dayjs(new Date()).tz('GMT+0')
-  // const activity22 = now.isBefore(date)
+  const date = dayjs.tz('2021-06-22 12:00', 'Africa/Accra')
+  const now = dayjs(new Date())
+  const activity22Active = now.isBefore(date)
 
-  // console.log('now', now)
-  // console.log('date', date)
-
-  return true
+  return activity22Active
 }
