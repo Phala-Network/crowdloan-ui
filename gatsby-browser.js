f15cc45c60ac1d3eb23f15788161139003421155
limichange <limichange@hotmail.com>
2021-12-28 14:44:28 +0800

    feat: redirect app page

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,5 +2,10 @@ const React = require('react')
 const App = require('./src/utils/wrapApp').default
 
 exports.wrapRootElement = ({ element }) => {
+  if (typeof window !== 'undefined') {
+    location.replace('https://app.phala.network/')
+    return null
+  }
+
   return <App>{element}</App>
 }
